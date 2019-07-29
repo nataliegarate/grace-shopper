eda445d6d7ef69af9de21857b42a8f2d16c7815e
Natalie Garate <nataliegarate@Natalies-MacBook-Air.local>
2019-07-29 17:03:37 -0400

    added cupcake table

--- server/auth/google.js
+++ server/auth/google.js
@@ -7,7 +7,7 @@ module.exports = router
 /**
  * For OAuth keys and other secrets, your Node process will search
  * process.env to find environment variables. On your production server,
- * you will be able to set these environment variables with the appropriate
+ * you will be able to set these environmennt variables with the appropriate
  * values. In development, a good practice is to keep a separate file with
  * these secrets that you only share with your team - it should NOT be tracked
  * by git! In this case, you may use a file called `secrets.js`, which will
