d2d91e0c3aac1eae72b8a277ad0ad75f2a9f5351
Eva E-P <eva.emendoerfer@gmail.com>
2019-07-30 12:14:24 -0400

    backend routes

--- server/api/cupcakes.js
+++ server/api/cupcakes.js
@@ -1,8 +1,21 @@
 const router = require('express').Router()
-const {Cupcake} = require('../db/index')
+const Cupcake = require('../db/models/cupcake')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
-  const cupcakes = await Cupcake.findAll()
-  res.send(cupcakes)
+  try {
+    const cupcakes = await Cupcake.findAll()
+    res.json(cupcakes)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const singleCupcake = await Cupcake.findOne({where: {id: req.params.id}})
+    res.json(singleCupcake)
+  } catch (err) {
+    next(err)
+  }
 })
