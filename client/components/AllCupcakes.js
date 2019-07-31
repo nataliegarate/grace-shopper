a8a9d1e49b376de5f8eb669b5f7f41e878e75912
julianaalbano <julianaalbano.us@gmail.com>
2019-07-31 13:02:51 -0400

    removed form from AllCupcakes and integrated local storage into OneCupcake

--- client/components/AllCupcakes.js
+++ client/components/AllCupcakes.js
@@ -2,24 +2,18 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {getAllCupcakes} from '../store/cupcake'
-import ls from 'local-storage'
 
 class AllCupcakes extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      localStorage: ls.get('name')
-    }
+    this.state = {}
   }
   componentDidMount() {
     this.props.getAllCupcakes()
-    ls.set('name', 'Hello there')
-
-    console.log('this is our this.state', this.state)
   }
+
   render() {
     const cupcakes = this.props.cupcakes
-
     return (
       <div>
         <h3>Cupcakes</h3>
@@ -27,16 +21,10 @@ class AllCupcakes extends React.Component {
           {cupcakes.map(cupcake => (
             <div key={cupcake.id} className="cupBox">
               <Link to={`/cupcakes/${cupcake.id}`}>
-                <img src={cupcake.imageUrl} />
+                <img className="cupcakes" src={cupcake.imageUrl} />
                 <p> {cupcake.name} </p>
               </Link>
-
               <p> {cupcake.price} $</p>
-
-              <form>
-                <input type="number" name="quantity" min="0" max="100" />
-                <input type="submit" name="addToCart" value="Add to Cart" />
-              </form>
             </div>
           ))}
         </div>
