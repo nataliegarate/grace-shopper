c6ab0c5c89653c43ab6d4697ce7f7cdaed666a10
Eva E-P <eva.emendoerfer@gmail.com>
2019-08-01 14:40:49 -0400

    created reducer for cart plus frontend post request

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -4,37 +4,39 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>Best Cupcakes by fantasticFour</h1>
-    <div id="links" />
-    <nav id="links">
-      <Link to="/cupcakes">Our cupcakes</Link>
-      <Link to="/home">Your profile</Link>
-      <div id="cartLink">
-        <Link to="/cart">Cart</Link>
-        <img className="cart" src="/images/shopping_bag.png" />
-      </div>
+const Navbar = ({handleClick, isLoggedIn}, props) => {
+  return (
+    <div>
+      <h1>Best Cupcakes by fantasticFour</h1>
+      <div id="links" />
+      <nav id="links">
+        <Link to="/cupcakes">Our cupcakes</Link>
+        <Link to="/home">Your profile</Link>
+        <div id="cartLink">
+          <Link to="/cart">Cart</Link>
+          <img className="cart" src="/images/shopping_bag.png" />
+        </div>
 
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
-)
+        {isLoggedIn ? (
+          <div>
+            {/* The navbar will show these links after you log in */}
+            <Link to="/home">Home</Link>
+            <a href="#" onClick={handleClick}>
+              Logout
+            </a>
+          </div>
+        ) : (
+          <div>
+            {/* The navbar will show these links before you log in */}
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
+          </div>
+        )}
+      </nav>
+      <hr />
+    </div>
+  )
+}
 
 /**
  * CONTAINER
--- client/store/index.js
+++ client/store/index.js
@@ -4,8 +4,8 @@ import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
 import cupcake from './cupcake'
-
-const reducer = combineReducers({user, cupcake})
+import cart from './cart'
+const reducer = combineReducers({user, cupcake, cart})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
@@ -14,3 +14,4 @@ const store = createStore(reducer, middleware)
 export default store
 export * from './user'
 export * from './cupcake'
+export * from './cart'
