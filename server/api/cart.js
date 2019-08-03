dc7f2eb59faffbe911a2ef1fed1d031b550db3ce
julianaalbano <julianaalbano.us@gmail.com>
2019-08-03 12:07:23 -0400

    updated guest functionality in backend routes, not final

--- server/api/cart.js
+++ server/api/cart.js
@@ -10,65 +10,96 @@ module.exports = router
 //GET request to retrieve a user's cart based on their order id
 router.get('/', async (req, res, next) => {
   try {
-    const userId = req.user.id
-    const userOrder = await Order.findOne({
-      where: {
-        userId,
-        completed: false
+    if (req.user === undefined) {
+      console.log('REQ.SESSION IS: ', req.session)
+    } else {
+      const userId = req.user.id
+      if (userId) {
+        const userOrder = await Order.findOne({
+          where: {
+            userId,
+            completed: false
+          }
+        })
+        const orderId = userOrder.id
+        const cupcakesInOrder = await OrderCupcake.findAll({
+          where: {
+            orderId
+          }
+        })
+        let cupcakeArr = []
+        for (let i = 0; i < cupcakesInOrder.length; i++) {
+          let obj = cupcakesInOrder[i]
+          let cupcakeId = obj.cupcakeId
+          let quantity = obj.quantity
+          let cupcake = await Cupcake.findByPk(cupcakeId)
+          cupcake.dataValues.quantity = quantity
+          cupcakeArr.push(cupcake)
+        }
+        res.json(cupcakeArr)
       }
-    })
-    const orderId = userOrder.id
-    const cupcakesInOrder = await OrderCupcake.findAll({
-      where: {
-        orderId
-      }
-    })
-    let cupcakeArr = []
-    for (let i = 0; i < cupcakesInOrder.length; i++) {
-      let obj = cupcakesInOrder[i]
-      let cupcakeId = obj.cupcakeId
-      let quantity = obj.quantity
-      let cupcake = await Cupcake.findByPk(cupcakeId)
-      cupcake.dataValues.quantity = quantity
-      cupcakeArr.push(cupcake)
     }
-    res.json(cupcakeArr)
   } catch (err) {
     next(err)
   }
 })
 
-//POST request to add an item to a user's cart
+//POST request to add an item to a user or guest cart
 router.post('/', async (req, res, next) => {
   try {
-    const userId = req.user.id
-    let foundOrder = await Order.findOne({
-      where: {
-        completed: false,
-        userId
+    if (req.user === undefined) {
+      if (req.session.cart === undefined) {
+        req.session.cart.hello = 'enida'
+        console.log(req.session.cart.hello)
+        console.log('you made it')
+        const cupcakeFromDb = await Cupcake.findByPk({
+          where: {
+            id: req.body.cupcakeId
+          }
+        })
+        // console.log(cupcakeFromDb)
+        req.session.cart.cupcakeIdOnCart = cupcakeFromDb
+        req.session.cart.cupcakeIdOnCart.quantity = req.body.quantity
+      } else {
+        let cupcakeIdOnCart = req.body.cupcakeId
+        if (req.session.cart.cupcakeIdOnCart) {
+          req.session.cart.cupcakeIdOnCart.dataValues.quantity =
+            req.body.quantity
+        } else {
+          const cupcakeFromDb = await Cupcake.findByPk(req.body.cupcakeId)
+          req.session.cart.cupcakeIdOnCart = cupcakeFromDb
+          req.session.cart.cupcakeIdOnCart.quantity += req.body.quantity
+        }
       }
-    })
-    if (!foundOrder) {
-      foundOrder = await Order.create({userId})
-    }
-
-    const cupcakeToUpdate = await OrderCupcake.findOne({
-      where: {
-        cupcakeId: req.body.cupcakeId
-      }
-    })
-    if (cupcakeToUpdate) {
-      const newQuantity =
-        Number(cupcakeToUpdate.quantity) + Number(req.body.quantity)
-      cupcakeToUpdate.update({quantity: newQuantity})
-      res.json(cupcakeToUpdate)
     } else {
-      const newOrderCupcake = await OrderCupcake.create({
-        quantity: req.body.quantity,
-        cupcakeId: req.body.cupcakeId,
-        orderId: foundOrder.id
+      const userId = req.user.id
+      let foundOrder = await Order.findOne({
+        where: {
+          completed: false,
+          userId
+        }
       })
-      res.json(newOrderCupcake)
+      if (!foundOrder) {
+        foundOrder = await Order.create({userId})
+      }
+      const cupcakeToUpdate = await OrderCupcake.findOne({
+        where: {
+          cupcakeId: req.body.cupcakeId
+        }
+      })
+      if (cupcakeToUpdate) {
+        const newQuantity =
+          Number(cupcakeToUpdate.quantity) + Number(req.body.quantity)
+        cupcakeToUpdate.update({quantity: newQuantity})
+        res.json(cupcakeToUpdate)
+      } else {
+        const newOrderCupcake = await OrderCupcake.create({
+          quantity: req.body.quantity,
+          cupcakeId: req.body.cupcakeId,
+          orderId: foundOrder.id
+        })
+        res.json(newOrderCupcake)
+      }
     }
   } catch (err) {
     next(err)
