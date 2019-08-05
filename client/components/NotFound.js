5ed3d4da275a66c347182ef3f54d47069963be86
julianaalbano <julianaalbano.us@gmail.com>
2019-08-05 17:37:38 -0400

    added not found component

---
+++ client/components/NotFound.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+      <h2>404 Error: Not Found</h2>
+      <img
+        src="https://media0.giphy.com/media/MW6bdII6g0lyM/source.gif"
+        id="sad-cupcake"
+      />
+      <br />
+    </div>
+  )
+}
+
+export default NotFound
