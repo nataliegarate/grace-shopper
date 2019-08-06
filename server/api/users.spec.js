7d03854161154d7853f6858180a7cade3fe00820
Natalie Garate <nataliegarate@Natalies-MacBook-Air.local>
2019-08-06 12:57:39 -0400

    test for security of user info

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -25,8 +25,7 @@ describe('User routes', () => {
         .get('/api/users')
         .expect(200)
 
-      expect(res.body).to.be.an('array')
-      expect(res.body[0].email).to.be.equal(codysEmail)
+      expect(res.body).to.be.equal('restricted content')
     })
   }) // end describe('/api/users')
 }) // end describe('User routes')
