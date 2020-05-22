58cd9940a8022a72aefedfd0b2a72d201400ce4d
nataliegarate <natalieegarate@hotmail.com>
2020-05-21 23:16:33 -0700

    basic view for desktop

--- client/components/CartView.js
+++ client/components/CartView.js
@@ -22,46 +22,52 @@ class CartView extends React.Component {
     }
     return (
       <div id="cart-view">
-        <h2>Your Shopping Cart</h2>
+        <h1 className="pink-title">Your Shopping Cart</h1>
         {this.props.order.map((cupcakeObj, i) => (
           <div key={i} className="single-cupcake-order">
-            <img className="group-cupcake-image" src={cupcakeObj.imageUrl} />
-            <div>
-              <p> Name: {cupcakeObj.name} </p>
-              <p> Quantity: {cupcakeObj.quantity} </p>
-              <button
-                type="submit"
-                className="nav-button"
-                onClick={() => this.props.deleteOrderThunk(cupcakeObj.id)}
-              >
-                Remove Order
-              </button>
+            <div className="left-single-item-cart">
+              <img className="group-cupcake-image" src={cupcakeObj.imageUrl} />
+              <div className="left-cart-info">
+                <p> Name: {cupcakeObj.name} </p>
+                <p> Quantity: {cupcakeObj.quantity} </p>
+                <button
+                  type="submit"
+                  className="nav-button"
+                  onClick={() => this.props.deleteOrderThunk(cupcakeObj.id)}
+                >
+                  Remove
+                </button>
+              </div>
             </div>
-            <div>
+            <div className="price">
               {' '}
-              <p> Price: {cupcakeObj.price}</p>
+              <p> Price: {cupcakeObj.price}.75</p>
             </div>
           </div>
         ))}
-        <button
-          type="button"
-          className="nav-button"
-          onClick={() => this.props.clearCartThunk()}
-        >
-          Empty Cart
-        </button>
-        <h3 id="total">Your Total: ${total(this.props.order)} </h3>
 
         {this.props.order.length > 0 && (
-          <div>
-            <br />
+          <button
+            id="empty-cart-button"
+            type="button"
+            className="nav-button"
+            onClick={() => this.props.clearCartThunk()}
+          >
+            Empty Cart
+          </button>
+        )}
+
+        <div id="total-and-checkout">
+          <h3 id="total">Your Total: ${total(this.props.order)}.75 </h3>
+
+          {this.props.order.length > 0 && (
             <Link to="/checkout">
-              <button type="button" className="nav-button">
+              <button type="button" id="checkout-button" className="nav-button">
                 Checkout
               </button>
             </Link>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     )
   }
