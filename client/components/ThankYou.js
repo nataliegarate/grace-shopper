04f874a38a90921302e4edf4f8f518682b07258d
nataliegarate <natalieegarate@hotmail.com>
2020-05-25 22:41:10 -0700

    thank you pahe

--- client/components/ThankYou.js
+++ client/components/ThankYou.js
@@ -2,13 +2,12 @@ import React from 'react'
 
 const ThankYou = () => {
   return (
-    <div>
+    <div id="thank-you-container">
       <img
         src="https://media3.giphy.com/media/l0MYyDa8S9ghzNebm/giphy.gif"
         id="thankYouImage"
       />
-      <br />
-      <h2 id="order-placed">Your order has been placed!</h2>
+      <h2 className="pink-title">Your order has been placed!</h2>
     </div>
   )
 }
